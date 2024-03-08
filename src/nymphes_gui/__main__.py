127099c1a41bda9df61badfb4c96e0401db726cc
Scott Lumsden <jtpack@gmail.com>
2024-03-08 18:31:39 -0500

    Moved kivy import inside __name__ == 'main' protection. This may have fixed the blank second window issue. Need to test on Windows...

--- src/nymphes_gui/__main__.py
+++ src/nymphes_gui/__main__.py
@@ -1,12 +1,7 @@
-from src.nymphes_gui.NymphesGuiApp import NymphesGuiApp
-from multiprocessing import set_start_method, freeze_support
-
-
-def main():
-    NymphesGuiApp().run()
-
 
 if __name__ == '__main__':
+    from src.nymphes_gui.NymphesGuiApp import NymphesGuiApp
+    from multiprocessing import set_start_method, freeze_support
     freeze_support()
     set_start_method('spawn')
-    main()
+    NymphesGuiApp().run()
