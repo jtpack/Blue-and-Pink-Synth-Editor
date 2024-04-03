7d0a479ffbda60913e25874edbb13bbe2dec7a5b
Scott Lumsden <jtpack@gmail.com>
2024-04-03 08:13:41 -0400

    Updated version string.

--- src/blue_and_pink_synth_editor/BlueAndPinkSynthEditorApp.py
+++ src/blue_and_pink_synth_editor/BlueAndPinkSynthEditorApp.py
@@ -37,7 +37,7 @@ from .nymphes_osc_process import NymphesOscProcess
 
 kivy.require('2.1.0')
 
-app_version_string = 'v0.1.8-beta'
+app_version_string = 'v0.1.9-beta'
 
 
 class BlueAndPinkSynthEditorApp(App):
