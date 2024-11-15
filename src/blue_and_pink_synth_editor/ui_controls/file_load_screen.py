76403b53ee315b22aef75c14401ec964f23d0133
Scott Lumsden <jtpack@gmail.com>
2024-11-15 07:22:13 -0500

    Getting started replacing the save file popup with a screen. Will do the load file dialog next. Then comes a screen for saving to preset slots.

---
+++ src/blue_and_pink_synth_editor/ui_controls/file_load_screen.py
@@ -0,0 +1,19 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen
+from pathlib import Path
+
+# Import the kv file with the same name as this file
+parent_directory = Path(__file__).resolve().parent
+this_file_name = Path(__file__).stem
+Builder.load_file(str(parent_directory / f'{this_file_name}.kv'))
+
+
+class FileLoadScreen(Screen):
+    pass
+
+
+# class FileLoadMainControlsBox(BoxLayout):
+#     screen_name = StringProperty('')
+#     corner_radius = NumericProperty(0)
